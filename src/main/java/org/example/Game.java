5724bca023d9eae2a558f834d07624859c42ee6b
Francisco <francisco.romero@cesurformacion.com>
2025-10-28 21:00:33 +0100

    Cambios

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Game {
+    private Integer id;
+    private String title;
+    private String platform;
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static Game mapper(ResultSet rs) throws SQLException {
+        return new Game(rs.getInt(1), rs.getString(2), rs.getString(3) );
+    }
+
+    public static void main(String[] args) {
+
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setServerName("localhost");
+        ds.setPort(3306);
+        ds.setUser(System.getenv("MYSQL_USER"));
+        ds.setPassword( System.getenv("MYSQL_PASSWORD"));
+        ds.setDatabaseName("ad");
+
+        try( Connection conn = ds.getConnection()){
+
+            PreparedStatement st = conn.prepareStatement("SELECT * FROM games WHERE platform LIKE ?");
+            st.setString(1, "PC");
+            ResultSet results = st.executeQuery();
+
+            ArrayList<Game> listado = new ArrayList<>();
+
+            while(results.next()){
+                listado.add( mapper(results) );
+            }
+
+            for(Game g : listado){
+                System.out.println(g);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
